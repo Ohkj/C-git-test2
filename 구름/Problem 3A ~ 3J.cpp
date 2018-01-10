daf04bb8fd284b3b3dd93df91a8b25974d0a40d9
오경진 <kjsjoh@naver.com>
2018-01-10 19:43:32 +0900

    18/01/10 update

--- 구름/Problem 3A ~ 3J.cpp
+++ 구름/Problem 3A ~ 3J.cpp
@@ -220,3 +220,184 @@ int main() {
 ******************/
 
 
+/*
+//문제3F - 과유불급(테스트 케이스 이외 에러...Why?????)
+#pragma warning(disable:4996)
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#include<iostream>
+#include<map>
+#include<set>
+#include<queue>
+#include<string>
+#include<string.h> 
+#include<functional>
+#include<cmath>
+using namespace std;
+const int MAX_N = 100000;
+long long rs[MAX_N+1];
+int v[MAX_N+1];
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	//vector<int> v(n,0);
+	for (int i = 1; i <= n; i++) {
+		//int num;
+		scanf("%d", &v[i]);
+		//cin >> v[i];
+	}
+	
+	rs[0] = 0;
+	
+	for(int i = 1; i <= n; i++){
+		rs[i] = rs[i-1] + v[i];	
+	} 
+	
+	long long maxn = -1;
+	int order = -1;
+	
+	for (int i = 0; i < m; i++) {
+		int L, R;
+		cin >> L >> R;
+			long long sum = rs[R] - rs[L-1];
+		
+		if (maxn < sum) {
+			maxn = sum;
+			order = i+1;
+		}
+	}
+	//printf("%d %lld\n", order, maxn);
+	cout << order << ' ' << maxn << '\n';
+
+	return 0;
+}
+**************/
+
+
+/*
+//문제3G - 팬미팅!!!!!!!!!!!!!!!!!!!!!(벡터 크기 지정 주의, 연속한 k개만큼 옆으로 이동)
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#include<iostream>
+#include<map>
+#include<set>
+#include<queue>
+#include<string>
+#include<string.h> 
+#include<functional>
+#include<cmath>
+using namespace std;
+//int v[200000]; // 벡터 안쓰면 전역 배열로 0으로 초기화
+//int v2[1000000]; // v2가 생년월일 저장 배열
+
+int main() {
+	int n, k;
+	
+	cin >> n >> k;
+	vector<int> v(n); // 벡터 크기 지정해야 에러 안남
+	vector<int> v2(1000000); // 생년월일 000000부터 991231까지. 크기 지정해야 에러 안남
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> v[i]; // 크기 지정하면 이 방식으로 입력
+		//v.push_back(num);
+	}
+	int cnt = 0;
+	int answer = 0;
+	for (int i = 0; i < n; i++) {
+		int b = v[i];
+		v2[b]++;
+		if (v2[b] == 1) { // 1이라는 소리가 처음 나온 것
+			cnt++;
+		}
+
+		if (i >= k) { // i >= k 조건 주의
+			int rb = v[i - k]; // 가장 앞에꺼 삭제
+			v2[rb]--;
+			if (v2[rb] == 0) { // 0이 되었다는 것은 해당 생년월일이 없어진 것이므로 cnt 감소
+				cnt--;
+			}
+		}
+
+		if (i >= k - 1) { // i >= k-1 조건 주의
+			if (cnt == k)
+				answer++;
+		}
+	}
+	cout << answer << '\n';
+
+	return 0;
+}
+*********************/
+
+
+//문제3H
+
+
+
+/*
+//문제3I - 세 카드
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#include<iostream>
+#include<map>
+#include<set>
+#include<queue>
+#include<string>
+#include<string.h> 
+#include<functional>
+#include<cmath>
+using namespace std;
+
+int main() {
+	int n, m;
+	//int v[1000]; // 배열버전
+	vector<int> v;
+	vector<int> v2;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		int vd;
+		cin >> vd;
+		v.push_back(vd);
+		//cin >> v[i]; // 배열버전
+		//scanf("%d", &v[i]);
+	}
+	//sort(v, v+n);
+	sort(v.begin(), v.end()); // binary_search 사용하려면 정렬해야 됨
+	for (int i = 0; i < m; i++) {
+		int num;
+		cin >> num;
+		bool flag = false;
+		for (int a = 0; a < n; a++) {
+			for (int b = 0; b < n; b++) {
+				int result = num - v[a] - v[b];
+				if(result < v[0] || result > v[n-1])
+					continue;
+				if (binary_search(v.begin(), v.end(), result)) { // 세 개의 숫자중 result가 있으면
+					flag = true;
+					break;
+				}
+			}
+			if (flag == true)
+				break;
+		}
+
+		if (flag == true)
+			v2.push_back(num);
+	}
+	sort(v2.begin(), v2.end());
+	if (v2.size() == 0)
+		cout << "NO";
+	else {
+		for (int i = 0; i < v2.size(); i++) {
+			cout << v2[i] << ' ';
+		}
+	}
+	//cout << '\n';
+
+	return 0;
+}
+*****************/
