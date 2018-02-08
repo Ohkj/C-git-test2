6a2f4d927f1129f5c47f1f7aa9a5a82282e9e6d7
오경진 <kjsjoh@naver.com>
2018-02-08 23:29:32 +0900

    18/02/08 update

--- 구름/Problem 5A ~ 5G.cpp
+++ 구름/Problem 5A ~ 5G.cpp
@@ -69,16 +69,16 @@ void testcase()
 		int cnt = 0;
 		for(int c = 0; c < n; c++){ // 모든 열에 대해
 				for(int dr = 0; dr < k; dr++){
-					cnt += space[r+dr][c];
+					cnt += space[r+dr][c]; // [r+dr]이 행을 k칸까지 제한(정사각형에서 세로크기).세로크기를 k로 유지하면서 열끝까지 확인
 				}
-				if(c >= k){ // n=5, k=3일 때 c=0,1,2,3...진행하면 c=3일 때 
+				if(c >= k){ // n=5, k=3일 때 c=0,1,2,3..k-1, k와 같이 진행하므로 c=k이면 k칸을 벗어난 경우(k-1까지가 k칸임)
 					for(int dr = 0; dr < k; dr++){
-						cnt -= space[r+dr][c-k]; // 앞부분 장애물 개수 빼기
+						cnt -= space[r+dr][c-k]; // 앞부분 장애물 개수 빼기. 앞부분 빼야 개수가 k개로 됨
 					}
 				}
-				if(c >= k-1){ // c=0,1,2일 경우
+				if(c >= k-1){ // c=0,1,2일 경우. k-1일 때가 k칸이고 위에서 cnt 변수에 배열 1, 0 배열 입력받은 것을 더하고 뺐으므로
 					if(answer > cnt) // 최소값 갱신
-							answer = cnt;
+							answer = cnt; // k-1일 때(k칸될때) k*k정사각형 안의 지뢰 개수를 카운트
 				}
 		}		
 	}
@@ -94,5 +94,6 @@ int main() {
 	
   return 0;
 }
+
 **************/
 
---
+++ 구름/Problem 5H ~ 5k.cpp
@@ -0,0 +1,116 @@
+
+/*
+문제5H - 두 직사각형!!!!!!!!!!!!!!!!!!!!
+
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int get_area(int la,int ra,int ba,int ta,int lb,int rb,int bb,int tb){
+	int lr,rr,br,tr; // 겹치는 사각형의 좌표
+	lr = max(la,lb); // 겹치는 사각형의 왼쪽좌표는 a,b 두개 사각형의 왼쪽좌표중 큰값
+	rr = min(ra,rb); // 겹치는 사각형의 오른쪽 좌표는 a,b 두개 사각형의 오른쪽 좌표중 작은값
+	br = max(ba,bb);
+	tr = min(ta,tb);
+	
+	if(lr <= rr && br <= tr){
+		return (rr-lr) * (tr-br);	
+	}
+	return 0;
+}
+
+void testcase(){
+	int ax,ay,bx,by;
+	int px,py,qx,qy;
+	
+	//두 개 사각형의 각 좌표를 설정
+	cin >> ax >> ay >> bx >> by >> px >> py >> qx >> qy;
+	int la, ra, ba, ta;
+	la = min(ax,bx);
+	ra = max(ax,bx);
+	ba = min(ay,by);
+	ta = max(ay,by);
+	
+	int lb, rb, bb, tb;
+	lb = min(px,qx);
+	rb = max(px,qx);
+	bb = min(py,qy);
+	tb = max(py,qy);
+	int result = get_area(la,ra,ba,ta,lb,rb,bb,tb);
+	
+	cout << result;
+}
+
+
+int main() {
+	int tc;
+	cin >> tc;
+	for(int i = 0 ; i < tc; i++){
+		testcase();	
+	}
+	
+  return 0;
+}
+
+//두 사각형일 경우만 가능
+***********************/
+
+
+/*
+//문제 5I - 두 선분
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+bool check(int la,int ra,int ba,int ta,int lb,int rb,int bb,int tb){
+	int lr,rr,br,tr; // 겹치는 사각형의 좌표
+	lr = max(la,lb); // 겹치는 사각형의 왼쪽좌표는 a,b 두개 사각형의 왼쪽좌표중 큰값
+	rr = min(ra,rb); // 겹치는 사각형의 오른쪽 좌표는 a,b 두개 사각형의 오른쪽 좌표중 작은값
+	br = max(ba,bb);
+	tr = min(ta,tb);
+	
+	if(lr <= rr && br <= tr){
+		return true; // 선분을 넓이가 0인것으로 봄
+	}
+	return false;
+}
+
+void testcase(){
+	int ax,ay,bx,by;
+	int px,py,qx,qy;
+	
+	//두 개 사각형의 각 좌표를 설정
+	cin >> ax >> ay >> bx >> by >> px >> py >> qx >> qy;
+	int la, ra, ba, ta;
+	la = min(ax,bx);
+	ra = max(ax,bx);
+	ba = min(ay,by);
+	ta = max(ay,by);
+	
+	int lb, rb, bb, tb;
+	lb = min(px,qx);
+	rb = max(px,qx);
+	bb = min(py,qy);
+	tb = max(py,qy);
+	bool result = check(la,ra,ba,ta,lb,rb,bb,tb);
+	
+	if(result == true){
+		cout << "YES" << endl;	
+	}
+	else{
+		cout << "NO" << endl;	
+	}
+
+}
+int main() {
+	int tc;
+	cin >> tc;
+	for(int i = 0 ; i < tc; i++){
+		testcase();	
+	}
+	
+  return 0;
+}
+*******************/
+
+
